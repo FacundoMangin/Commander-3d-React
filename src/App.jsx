961e85cea644552ca1d7ee953510f5683c87c494
Facundo Mangin <facundo.mangin@gmail.com>
2023-10-16 21:10:54 -0300

    solucion para deployed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './App.css'
+import './app.css'
 
 import Router from './components/routes/Router'
 import './components/css/main.css';
